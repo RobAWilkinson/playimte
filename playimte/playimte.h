2c5a4cf7d88c44599619f85aa591f7ca492bdadf
Rob Wilkinson <wilkinson.robert.a@gmail.com>
2016-05-14 12:37:10 -0700

    Initial Commit

---
+++ playimte/playimte.h
@@ -0,0 +1,19 @@
+//
+//  playimte.h
+//  playimte
+//
+//  Created by Robert Wilkinson on 5/14/16.
+//  Copyright © 2016 Robert WIlkinson. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for playimte.
+FOUNDATION_EXPORT double playimteVersionNumber;
+
+//! Project version string for playimte.
+FOUNDATION_EXPORT const unsigned char playimteVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <playimte/PublicHeader.h>
+
+
